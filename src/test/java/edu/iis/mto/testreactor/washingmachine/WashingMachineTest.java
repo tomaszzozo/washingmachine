cecd92d2ded4a3dd7a750989ea4a4fc996b5ea95
Tomasz Wojtkiewicz <222616@edu.p.lodz.pl>
2022-05-06 18:19:05 +0200

    Pierwszy test

--- src/test/java/edu/iis/mto/testreactor/washingmachine/WashingMachineTest.java
+++ src/test/java/edu/iis/mto/testreactor/washingmachine/WashingMachineTest.java
@@ -1,32 +1,42 @@
 package edu.iis.mto.testreactor.washingmachine;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @ExtendWith(MockitoExtension.class)
 class WashingMachineTest {
 
-    @Mock
-    private DirtDetector dirtDetector;
-    @Mock
-    private Engine engine;
-    @Mock
-    private WaterPump waterPump;
-    private WashingMachine washingMashine;
+    @Mock private DirtDetector dirtDetector;
+    @Mock private Engine engine;
+    @Mock private WaterPump waterPump;
+    private WashingMachine washingMachine;
 
     @BeforeEach
-    void setUp() throws Exception {
-        washingMashine = new WashingMachine(dirtDetector, engine, waterPump);
+    void setUp() {
+        washingMachine = new WashingMachine(dirtDetector, engine, waterPump);
     }
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void properBatchWithStaticProgram() {
+        LaundryBatch laundryBatch = LaundryBatch.builder()
+                .withMaterialType(Material.COTTON)
+                .withWeightKg(7d)
+                .build();
+        ProgramConfiguration programConfiguration = ProgramConfiguration.builder()
+                .withProgram(Program.LONG)
+                .withSpin(true)
+                .build();
+        LaundryStatus result = washingMachine.start(laundryBatch, programConfiguration);
+        assertEquals(result, LaundryStatus.builder()
+                .withErrorCode(ErrorCode.NO_ERROR)
+                .withResult(Result.SUCCESS)
+                .withRunnedProgram(Program.LONG)
+                .build());
     }
 
 }
